d5b1cb8b1bd0ca2e5a533fb03c2b406c20d7c3a9
kirka50 <75656501+kirka50@users.noreply.github.com>
2023-12-04 20:06:17 +0500

    work

--- KirillsGegCounter/app/src/main/java/com/example/kirillsgegcounter/MainActivity.kt
+++ KirillsGegCounter/app/src/main/java/com/example/kirillsgegcounter/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.kirillsgegcounter
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.Firebase
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
-    var gegs: Int = 5
+    var gegs: Int = 0
+    private val database = Firebase.database
+    private val myRef = database.getReference("gegs")
+
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +27,33 @@ class MainActivity : AppCompatActivity() {
                 Log.d("BUTTONS", "click")
                 incraseCount(view)
             }
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<Int>()
+                Log.d(TAG, "Value is: $value")
+                if (value != null) {
+                    gegs = value
+                    view.setText(gegs.toString())
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
     fun incraseCount(view: TextView) {
         this.gegs = this.gegs + 1
         Log.d("wtf", this.gegs.toFloat().toString())
         view.text = this.gegs.toString()
+        myRef.setValue(gegs)
     }
+    fun refresgGegs(Gegs: Int) {
+        this.gegs = Gegs.toInt()
+    }
+
+
 }
